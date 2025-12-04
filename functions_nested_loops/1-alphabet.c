417651e633fca38592befa4049b6147cdd1c6ac9
Uzeyir44 <Uzeyir44>
2025-12-04 14:53:33 +0000

    task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <main.h>
+
+void print_alphabet(void);
+
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
+
+void print_alphabet(void)
+{
+    char c;
+
+    for (c = 'a'; c <= 'z'; c++)
+    {
+        _putchar(c);
+    }
+    _putchar('\n');
+    return;
+}
