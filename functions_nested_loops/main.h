72ca21531edfcd31383eaee084b9fb1c23c3b44c
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-05 12:40:40 +0000

    task 4

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -5,5 +5,6 @@ int _putchar(char c);  /* prototype for the provided function */
 void print_alphabet(void); /* prototype for print_alphabet */
 void print_alphabet_x10(void); /* alphabet 10 times */
 int _islower(int c); /* find lower case */
+int _isalpha(int c); /* find lower and upper case */
 
 #endif
