dad66b0769426ebf4c30b0efd42116123ae39863
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-05 13:53:28 +0000

    task 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,56 +1,61 @@
 #include "main.h"
 
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ * Return: void
+ */
 void jack_bauer(void)
 {
-    int hour;
-    int minute;
+	int hour;
+	int minute;
 
-    for (hour = 0; hour < 24; hour++)
-    {
-        for (minute = 0; minute < 60; minute++)
-        {
-            if (hour < 10)
-            {
-                if (minute < 10)
-                {
-                    _putchar('0');
-                    _putchar('0' + hour);
-                    _putchar(':');
-                    _putchar('0');
-                    _putchar('0' + minute);
-                    _putchar('\n');
-                }
-                else
-                {
-                    _putchar('0');
-                    _putchar('0' + hour);
-                    _putchar(':');
-                    _putchar('0' + (minute / 10));
-                    _putchar('0' + (minute % 10));
-                    _putchar('\n');
-                }
-            }
-            else
-            {
-                if (minute < 10)
-                {
-                    _putchar('0' + (hour / 10));
-                    _putchar('0' + (hour % 10));
-                    _putchar(':');
-                    _putchar('0');
-                    _putchar('0' + minute);
-                    _putchar('\n');
-                }
-                else
-                {
-                    _putchar('0' + (hour / 10));
-                    _putchar('0' + (hour % 10));
-                    _putchar(':');
-                    _putchar('0' + (minute / 10));
-                    _putchar('0' + (minute % 10));
-                    _putchar('\n');
-                }
-            }
-        }
-    }
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			if (hour < 10)
+			{
+				if (minute < 10)
+				{
+					_putchar('0');
+					_putchar('0' + hour);
+					_putchar(':');
+					_putchar('0');
+					_putchar('0' + minute);
+					_putchar('\n');
+				}
+				else
+				{
+					_putchar('0');
+					_putchar('0' + hour);
+					_putchar(':');
+					_putchar('0' + (minute / 10));
+					_putchar('0' + (minute % 10));
+					_putchar('\n');
+				}
+			}
+			else
+			{
+				if (minute < 10)
+				{
+					_putchar('0' + (hour / 10));
+					_putchar('0' + (hour % 10));
+					_putchar(':');
+					_putchar('0');
+					_putchar('0' + minute);
+					_putchar('\n');
+				}
+				else
+				{
+					_putchar('0' + (hour / 10));
+					_putchar('0' + (hour % 10));
+					_putchar(':');
+					_putchar('0' + (minute / 10));
+					_putchar('0' + (minute % 10));
+					_putchar('\n');
+				}
+			}
+		}
+	}
 }
