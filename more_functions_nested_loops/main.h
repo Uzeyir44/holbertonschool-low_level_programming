6d34e612844f776c345734321acd569321305af9
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-05 15:55:37 +0000

    task 3

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -4,5 +4,6 @@
 int _isupper(int c); /* finds uppercase */
 int _isdigit(int c); /* finds number between 0-9 */
 int mul(int a, int b); /* multiplies two integers */
+void print_numbers(void); /* prints numbers from 0 to 9 */
 
 #endif 
