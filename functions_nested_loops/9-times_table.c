ced7f6c3cda0c057a34ed34403f672ef2dc1d9b9
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-05 14:20:41 +0000

    task 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+void times_table(void)
+{
+    int i;
+    int j;
+    int c;
+    int b;
+
+    c = 0;
+    for (i = 0; i <= 9; i++)
+    {
+        b = 0;
+        for (j = 0; i <= 9; j++)
+        {
+            if (j != 9)
+            {
+                if (b<10)
+                {
+                    _putchar('0' + b);
+                    _putchar(',');
+                    _putchar(' ');
+                    _putchar(' ');
+
+                }
+                else
+                {
+                    _putchar('0' + (b / 10));
+                    _putchar('0' + (b % 10));
+                    _putchar(',');
+                    _putchar(' ');
+                    _putchar(' ');
+                }
+            }
+            else
+            {
+                if (b<10)
+                {
+                    _putchar('0' + b);
+
+                }
+                else
+                {
+                    _putchar('0' + (b / 10));
+                    _putchar('0' + (b % 10));
+                }
+            }
+            b = b + c;
+            _putchar('\n');
+        }
+        c++;
+    }
+
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -10,5 +10,6 @@ int print_sign(int n); /* find is number positive, negative or 0*/
 int _abs(int); /* absolute value of number */
 int print_last_digit(int); /* prints the last digit */
 void jack_bauer(void); /* prints every minute of the day */
+void times_table(void); /* time table */
 
 #endif
