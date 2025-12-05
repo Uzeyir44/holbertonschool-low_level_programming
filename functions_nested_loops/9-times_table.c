778c7e57e313dce64ea48459ef6470a2969b4580
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-05 14:32:01 +0000

    task 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -11,7 +11,7 @@ void times_table(void)
     for (i = 0; i <= 9; i++)
     {
         b = 0;
-        for (j = 0; i <= 9; j++)
+        for (j = 0; j <= 9; j++)
         {
             if (j != 9)
             {
