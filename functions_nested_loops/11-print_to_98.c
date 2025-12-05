2cbfdbe31e2b08207c1a3f5083e8a31cf8eeafe2
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-05 15:08:06 +0000

    task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,15 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 
 void print_to_98(int n)
 {
     while (n <= 98)
     {
-        if (n>=10)
+        if (n != 98)
         {
-            _putchar('0' + (n / 10));
+            printf("%d, ", n);
         }
-
-        _putchar('0' + (n % 10));
+        else
+        {
+            printf("%d", n);
+        }
         n++;
     }
 }
