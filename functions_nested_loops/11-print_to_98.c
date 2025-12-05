ec5fdc50a11cc160bc4ea1576873eb1f44b79679
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-05 15:04:16 +0000

    task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -6,10 +6,10 @@ void print_to_98(int n)
     {
         if (n>=10)
         {
-            _putchar(n / 10);
+            _putchar('0' + (n / 10));
         }
 
-        _putchar(n % 10);
+        _putchar('0' + (n % 10));
         n++;
     }
 }
