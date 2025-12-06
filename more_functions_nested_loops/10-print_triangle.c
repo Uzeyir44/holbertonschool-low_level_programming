57b8cc7bf5ca5e65f41024c48f1791fe778335f6
Uzeyir44 <uzeirdzahangriov@gmail.com>
2025-12-06 21:35:45 +0000

    task 10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -6,6 +6,11 @@ void print_triangle(int size)
     int j;
     int k;
 
+    if (size <= 0)
+    {
+        _putchar('\n');
+    }
+    
     for (i = 1; i <= size; i++)
     {
         for (j = 0; j < (size-i); j++)
