c143c78b4468c752b4683eb702b240a413102807
Uzeyir44 <uzeirdzahangriov@gmail.com>
2025-12-06 20:44:08 +0000

    task 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+void print_diagonal(int n)
+{
+    int i;
+    int j;
+
+    for (i = 0; i < n; n++)
+    {
+        for (j = 0; j < 0; j++)
+        {
+            _putchar(' ');
+        }
+
+        _putchar('\\');
+    }
+
+    _putchar('\n');
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -9,5 +9,6 @@ void print_numbers(void); /* prints numbers from 0 to 9 */
 void print_most_numbers(void); /* print numbers expect 2 and 4*/
 void more_numbers(void); /* 10 times from 0 to 14 */
 void print_line(int n); /* print strait line */
+void print_diagonal(int n); /* prints the diogonal */
 
 #endif 
