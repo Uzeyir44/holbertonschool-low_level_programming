5a6f28569d7cad1177df484716950610969edb8b
Uzeyir44 <uzeirdzahangriov@gmail.com>
2025-12-06 21:36:54 +0000

    task 10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,28 +1,34 @@
 #include "main.h"
 
+/**
+ * print_triangle - prints a triangle of '#' characters
+ * @size: size of the triangle
+ *
+ * Return: void
+ */
 void print_triangle(int size)
 {
-    int i;
-    int j;
-    int k;
+	int i;
+	int j;
+	int k;
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-    }
-    
-    for (i = 1; i <= size; i++)
-    {
-        for (j = 0; j < (size-i); j++)
-        {
-            _putchar(' ');
-        }
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
 
-        for (k = 0; k < i; k++)
-        {
-            _putchar('#');
-        }
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 0; j < (size - i); j++)
+		{
+			_putchar(' ');
+		}
 
-        _putchar('\n');
-    }
+		for (k = 0; k < i; k++)
+		{
+			_putchar('#');
+		}
+
+		_putchar('\n');
+	}
 }
