cb174b2f27453cf453c435e323c12503b5097976
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-08 14:36:58 +0000

    task 2

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -3,5 +3,6 @@
 
 void reset_to_98(int *n); /* changes the value of variable */
 void swap_int(int *a, int *b); /* swaps the value of variables */
+int _strlen(char *s); /* returns the length of string */
 
 #endif
