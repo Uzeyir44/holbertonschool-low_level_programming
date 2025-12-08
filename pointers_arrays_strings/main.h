34e80f7301bbdfbd810a622093da8bce99091d59
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-08 14:45:23 +0000

    task 3

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,8 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);  /* prototype for the provided function */
 void reset_to_98(int *n); /* changes the value of variable */
 void swap_int(int *a, int *b); /* swaps the value of variables */
 int _strlen(char *s); /* returns the length of string */
+void _puts(char *str); /* prints string */
 
 #endif
