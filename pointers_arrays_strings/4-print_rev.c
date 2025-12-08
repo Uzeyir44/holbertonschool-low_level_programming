7a7576d81507061e3d049cd03b90effe2e36a206
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-08 14:51:31 +0000

    task 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+void print_rev(char *s)
+{
+    int i;
+
+    i = 0;
+
+    while (s[i] != '\0')
+    {
+        i++;
+    }
+
+    for (--i; i >= 0; i--)
+    {
+        _putchar(s[i]);
+    }
+
+    _putchar('\n');
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -6,5 +6,6 @@ void reset_to_98(int *n); /* changes the value of variable */
 void swap_int(int *a, int *b); /* swaps the value of variables */
 int _strlen(char *s); /* returns the length of string */
 void _puts(char *str); /* prints string */
+void print_rev(char *s); /* prints a string in reverse */
 
 #endif
