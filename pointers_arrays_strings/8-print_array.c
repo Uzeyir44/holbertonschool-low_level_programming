bbb2aa78beed8d5fc74adf9883aef58319476b13
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-09 14:26:42 +0000

    task 8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,21 +1,28 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: array to print
+ * @n: number of elements
+ *
+ * Return: void
+ */
 void print_array(int *a, int n)
 {
-    int i;
+	int i;
 
-    if (n > 0)
-    {
-        for (i = 0; i < n - 1; i++)
-        {
-            printf("%d, ", a[i]);
-        }
-    
-        printf("%d\n", a[n-1]);
-    }
-    else
-    {
-        printf("\n");
-    }
+	if (n > 0)
+	{
+		for (i = 0; i < n - 1; i++)
+		{
+			printf("%d, ", a[i]);
+		}
+
+		printf("%d\n", a[n - 1]);
+	}
+	else
+	{
+		printf("\n");
+	}
 }
