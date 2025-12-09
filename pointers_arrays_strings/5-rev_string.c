1520256968683e06728310f2de6e75415a7f96a3
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-09 13:28:48 +0000

    task 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,22 +1,27 @@
 #include "main.h"
 
+/**
+ * rev_string - reverses a string in place
+ * @s: string to reverse
+ *
+ * Return: void
+ */
 void rev_string(char *s)
 {
-    int i;
-    int c;
-    char a;
-    
-    i = 0;
+	int i;
+	int c;
+	char a;
 
-    while (s[i] != '\0')
-    {
-        i++;
-    }
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
 
-    for (c = 0; c < i/2; c++)
-    {
-        a = s[c];
-        s[c] = s[i-1-c];
-        s[i-1-c] = a;
-    }
+	for (c = 0; c < i / 2; c++)
+	{
+		a = s[c];
+		s[c] = s[i - 1 - c];
+		s[i - 1 - c] = a;
+	}
 }
