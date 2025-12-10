8c16dd7450287f099dd55d93cd7153718fc03511
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-10 12:35:18 +0000

    task 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,29 +1,37 @@
 #include "main.h"
 
+/**
+ * _strncpy - Copies a string
+ * @dest: Destination buffer
+ * @src: Source string
+ * @n: Number of bytes to copy
+ *
+ * Return: Pointer to dest
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
-    char *point = dest;
-    int i;
+	char *point = dest;
+	int i;
 
-    for (i = 0; i < n; i++)
-    {
-        if (src[i] != '\0')
-        {
-            *point = src[i];
-            point++;
-        }
-        else
-        {
-            break;
-        }
-    }
+	for (i = 0; i < n; i++)
+	{
+		if (src[i] != '\0')
+		{
+			*point = src[i];
+			point++;
+		}
+		else
+		{
+			break;
+		}
+	}
 
-    while (i < n)
-    {
-        *point = '\0';
-        point++;
-        i++;
-    }
-    
-    return (dest);
+	while (i < n)
+	{
+		*point = '\0';
+		point++;
+		i++;
+	}
+
+	return (dest);
 }
