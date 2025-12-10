9ec7dd8856f40a4d00d61be31835fa017a2be533
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-10 11:59:01 +0000

    task 1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,25 +1,33 @@
 #include "main.h"
 
+/**
+ * _strncat - concatenates two strings up to n bytes
+ * @dest: destination string
+ * @src: source string
+ * @n: maximum number of bytes to append
+ *
+ * Return: pointer to dest
+ */
 char *_strncat(char *dest, char *src, int n)
 {
-    char *point  = dest;
-    int i;
+	char *point = dest;
+	int i;
 
-    while (*point != '\0')
-    {
-        point++;
-    }
+	while (*point != '\0')
+	{
+		point++;
+	}
 
-    for (i = 0; i < n; i++)
-    {
-        *point = src[i];
-        point++;
+	for (i = 0; i < n; i++)
+	{
+		*point = src[i];
+		point++;
 
-        if (src[i] == '\0')
-        {
-            break;
-        }
-    }
+		if (src[i] == '\0')
+		{
+			break;
+		}
+	}
 
-    return (dest);
+	return (dest);
 }
