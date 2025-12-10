25eeef489eab4df5d783c0ca074c443b0b4f0f51
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-10 13:36:20 +0000

    task 5

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,20 +1,25 @@
 #include "main.h"
 
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @p: string to modify
+ *
+ * Return: pointer to the modified string
+ */
 char *string_toupper(char *p)
 {
-    int i;
+	int i;
 
-    i = 0;
+	i = 0;
 
-    while (p[i] != '\0')
-    {
-        if (p[i] >= 'a' && p[i] <= 'z')
-        {
-            p[i] = p[i] - 32;
-        }
+	while (p[i] != '\0')
+	{
+		if (p[i] >= 'a' && p[i] <= 'z')
+		{
+			p[i] = p[i] - 32;
+		}
+		i++;
+	}
 
-        i++;
-    }
-
-    return (p);
+	return (p);
 }
