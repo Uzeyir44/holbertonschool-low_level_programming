04737e22d34fc3f8cecf30f67bfee39d7fd21df1
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-10 12:33:50 +0000

    task 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -12,12 +12,17 @@ char *_strncpy(char *dest, char *src, int n)
             *point = src[i];
             point++;
         }
-        
         else
         {
-            *point = '\0';
-            point++;
+            break;
         }
+    }
+
+    while (i < n)
+    {
+        *point = '\0';
+        point++;
+        i++;
     }
     
     return (dest);
