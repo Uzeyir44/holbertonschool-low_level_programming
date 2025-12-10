d377d95ffe79e6644ef2d7d168b2f79072cecf15
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-10 13:04:03 +0000

    task 3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -5,31 +5,27 @@ int _strcmp(char *s1, char *s2)
     int sum;
     int i;
 
-    sum = 0;
     i = 0;
 
     while (s1[i] != '\0' && s2[i] != '\0')
     {
-        sum = sum + (s1[i] - s2[i]);
+        if (s1[i] != s2[i])
+        {
+            sum = s1[i] - s2[i];
+            return (sum);
+        }
+
         i++;
     }
 
     if (s1[i] == '\0')
     {
-        while (s2[i] != '\0')
-        {
-            sum = sum + ('\0' - s2[i]);
-            i++;
-        }
+        sum = '\0' - s2[i];
     }
     else
     {
-        while (s1[i] != '\0')
-        {
-            sum = sum + ('\0' - s1[i]);
-            i++;
-        }
+        sum = '\0' - s1[i];
     }
-
+    
     return (sum);
 }
