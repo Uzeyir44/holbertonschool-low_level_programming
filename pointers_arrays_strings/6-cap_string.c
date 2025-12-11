734ccefd7dffc647d1713f130215e4befadf7f85
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-11 13:03:46 +0000

    task 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,24 +1,38 @@
 #include "main.h"
 
+/**
+ * cap_string - capitalizes all words of a string
+ * @p: pointer to the string
+ *
+ * Return: pointer to modified string
+ */
 char *cap_string(char *p)
 {
-    int i;
-    
-    i = 0;
+	int i;
 
-    while (p[i] != '\0')
-    {
-        if (i == 0 && p[i] >= 'a' && p[i] <= 'z')
-        {
-            p[i] = p[i] - 32;
-        }
-        else if ((p[i-1] == ' ' || p[i-1] == '\n' || p[i-1] == ',' || p[i-1] == '.' || p[i-1] == '?' || p[i-1] == '!' || p[i-1] == '(' || p[i-1] == ')' || p[i-1] == '{' || p[i-1] == '}' || p[i-1] == '"' || p[i-1] == '\t') && p[i] >= 'a' && p[i] <= 'z')
-        {
-            p[i] = p[i] - 32;
-        }
+	i = 0;
 
-        i++;
-    }
+	while (p[i] != '\0')
+	{
+		if (i == 0 && p[i] >= 'a' && p[i] <= 'z')
+		{
+			p[i] = p[i] - 32;
+		}
+		else if (
+			(p[i - 1] == ' ' || p[i - 1] == '\n' ||
+			p[i - 1] == ',' || p[i - 1] == '.' ||
+			p[i - 1] == '?' || p[i - 1] == '!' ||
+			p[i - 1] == '(' || p[i - 1] == ')' ||
+			p[i - 1] == '{' || p[i - 1] == '}' ||
+			p[i - 1] == '"' || p[i - 1] == '\t') &&
+			(p[i] >= 'a' && p[i] <= 'z')
+		)
+		{
+			p[i] = p[i] - 32;
+		}
 
-    return (p);
+		i++;
+	}
+
+	return (p);
 }
