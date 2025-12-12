ff7f71321901241bb1e53d8eb87065ee7c7833e8
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-12 20:29:37 +0000

    task 3 refined

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -3,12 +3,29 @@
 unsigned int _strspn(char *s, char *accept)
 {
     unsigned int i;
+    int j;
+    int g;
+    int check;
 
-    for (i = 0; accept[i] != '\0'; i++)
+    for (j = 0; accept[j] != '\0', j++)
     {
-        if (accept[i] != s[i])
+        j++;
+    }
+
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        check = 0;
+        for (g = 0; g <= j; g++)
         {
-            break;
+            if (s[i] == accept[g])
+            {
+                check = 1;
+            }
+        }
+
+        if (check == 0)
+        {
+            return(i)
         }
     }
 
