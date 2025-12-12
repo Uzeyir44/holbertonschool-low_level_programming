43c0ae400266d047609c60aab73d9126603df892
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-12 20:35:44 +0000

    semicolon error

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -7,7 +7,7 @@ unsigned int _strspn(char *s, char *accept)
     int g;
     int check;
 
-    for (j = 0; accept[j] != '\0', j++)
+    for (j = 0; accept[j] != '\0'; j++)
     {
         j++;
     }
