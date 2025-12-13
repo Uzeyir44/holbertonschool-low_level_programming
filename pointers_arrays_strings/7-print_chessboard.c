dcaf0ed7e5c9395152836f54181aa15f5bc49edf
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-13 10:27:13 +0000

    task 6

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_chessboard(char (*a)[8])
+{
+    int rows;
+    int columns;
+    
+    for (rows = 0; rows < 8; rows++)
+    {
+        for (columns = 0; columns < 8; columns++)
+        {
+            _putchar(a[rows][columns]);
+        }
+        _putchar('\n')
+    }
+}
