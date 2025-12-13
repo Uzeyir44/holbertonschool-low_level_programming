3a52f7c08d4730873a8eae3a84d925b6635fc926
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-13 11:04:26 +0000

    error correction

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -6,13 +6,14 @@ void print_diagsums(int *a, int size)
     int sum1;
     int sum2;
     int i;
+    int j;
 
     for (i = 0; i < size * size; i = i + size + 1)
     {
         sum1 = sum1 + a[i];
     }
 
-    for (i = size * size - size + 1; i >= 0; i = i - size + 1)
+    for (j = size * size - size + 1; i > 0; i = i - size + 1)
     {
         sum2 = sum2 + a[i];
     }
