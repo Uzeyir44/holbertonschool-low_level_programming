a589d9aa59fb35a3e4122e64daed96783c0f6d17
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-15 14:51:21 +0000

    task 3

--- recursion/main.h
+++ recursion/main.h
@@ -5,5 +5,6 @@ int _putchar(char c);  /* prototype for the provided function */
 void _puts_recursion(char *s); /* prints string using recursion */
 void _print_rev_recursion(char *s); /* prints string in reverse by recursion */
 int _strlen_recursion(char *s); /* returns the length of string using recursion */
+int factorial(int n); /* factorial using recursion */
 
 #endif
