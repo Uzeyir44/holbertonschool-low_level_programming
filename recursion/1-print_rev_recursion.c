af97048b3ab7a8c05c36149695bd0456a123cbf5
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-15 14:41:17 +0000

    betty

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: pointer to the string
+ */
 void _print_rev_recursion(char *s)
 {
-    if (*s != '\0')
-    {
-        _print_rev_recursion(s+1);
-    }
-    else
-    {
-        return;
-    }
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+	}
+	else
+	{
+		return;
+	}
 
-    _putchar(*s);
-    
+	_putchar(*s);
 }
