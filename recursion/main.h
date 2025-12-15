1f6f73efa38c7def80d0e80f6eaa15a69573b18b
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-15 14:58:22 +0000

    task 4

--- recursion/main.h
+++ recursion/main.h
@@ -6,5 +6,6 @@ void _puts_recursion(char *s); /* prints string using recursion */
 void _print_rev_recursion(char *s); /* prints string in reverse by recursion */
 int _strlen_recursion(char *s); /* returns the length of string using recursion */
 int factorial(int n); /* factorial using recursion */
+int _pow_recursion(int x, int y); /* return the value of x rised to the power y via recursion */
 
 #endif
