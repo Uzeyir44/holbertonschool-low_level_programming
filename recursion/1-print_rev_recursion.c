43ba946a29d3959725639e9cc0cfdcfdbaeb5db0
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-15 14:39:49 +0000

    more efficient

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -6,11 +6,11 @@ void _print_rev_recursion(char *s)
     {
         _print_rev_recursion(s+1);
     }
-
-    _putchar(*s);
-    
-    if (*s == '\n')
+    else
     {
         return;
     }
+
+    _putchar(*s);
+    
 }
