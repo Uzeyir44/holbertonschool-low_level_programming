3ec79906e8d3c08a3dfe4e83e5926fc9f9140227
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-15 14:11:04 +0000

    error correction

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -4,10 +4,10 @@ void _puts_recursion(char *s)
 {
     if (*s == '\0')
     {
-        return ('\n')
+        return ('\n');
     }
 
     _putchar(*s);
 
-    return (_puts_recursion(s+1));
+    _puts_recursion(s+1);
 }
