9808acbcd0c201c403406d21fa76c1ba04e77f7c
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-15 14:29:47 +0000

    task 1

--- recursion/main.h
+++ recursion/main.h
@@ -3,5 +3,6 @@
 
 int _putchar(char c);  /* prototype for the provided function */
 void _puts_recursion(char *s); /* prints string using recursion */
+void _print_rev_recursion(char *s); /* prints string in reverse by recursion */
 
 #endif
