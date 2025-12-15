4ff12c35f671b48c45baed5ee310a0dcaa00c6f8
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-15 14:33:33 +0000

    without '\0'

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -7,9 +7,11 @@ void _print_rev_recursion(char *s)
         _print_rev_recursion(s+1);
     }
 
-    _putchar(*s);
-    if (*s == '\n')
+    if (*(s-1) == '\n')
     {
+        _putchar('\n');
         return;
     }
+
+    _putchar(*(s-1));
 }
