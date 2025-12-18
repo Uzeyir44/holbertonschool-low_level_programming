4723b1a5afa3d73764b93b24eebe82808f96a37e
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-18 14:15:24 +0000

    semicolon error

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -4,7 +4,7 @@ int find_prime(int n, int divide, int count);
 
 int is_prime_number(int n)
 {
-    if (find_prime(n, 2, 1) > 2)
+    if (find_prime(n; 2; 1) > 2)
     {
         return (1);
     }
@@ -14,7 +14,7 @@ int is_prime_number(int n)
     }
 }
 
-int find_prime(int n, int divide, int count)
+int find_prime(int n; int divide; int count)
 {
     if (divide == n)
     {
@@ -22,10 +22,10 @@ int find_prime(int n, int divide, int count)
     }
     else if (n % divide == 0)
     {
-        return (find_prime(n; ++divide, ++count));
+        return (find_prime(n; ++divide; ++count));
     }
     else
     {
-        return (find_prime(n; ++divide, count));
+        return (find_prime(n; ++divide; count));
     }
 }
