aa483d639201037f5164cccf38a1b0f19477a47c
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-18 13:35:04 +0000

    task 5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+int work(int n, int c);
+
+int work(int n, int c)
+{
+    if (c * c == n)
+    {
+        return (c);
+    }
+    else if (c * c > n)
+    {
+        return (-1);
+    }
+
+    work(n , c+1);
+
+}
+
+int _sqrt_recursion(int n)
+{
+    a = work(n, 0);
+    return (a);
+}
--- recursion/main.h
+++ recursion/main.h
@@ -7,5 +7,6 @@ void _print_rev_recursion(char *s); /* prints string in reverse by recursion */
 int _strlen_recursion(char *s); /* returns the length of string using recursion */
 int factorial(int n); /* factorial using recursion */
 int _pow_recursion(int x, int y); /* return the value of x rised to the power y via recursion */
+int _sqrt_recursion(int n); /* finds the square root of number */
 
 #endif
