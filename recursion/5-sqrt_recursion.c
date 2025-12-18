d414ff7480ce3394357a67e02063957817591824
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-18 13:42:26 +0000

    betty

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -4,16 +4,16 @@ int sqrt_helper(int n, int guess);
 
 /**
  * _sqrt_recursion - returns the natural square root of a number
- * @n: number to find the square root of
+ * @n: number to calculate the square root of
  *
- * Return: natural square root, or -1 if none exists
+ * Return: the natural square root, or -1 if none exists
  */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return (-1);
+	if (n < 0)
+		return (-1);
 
-    return (sqrt_helper(n, 1));
+	return (sqrt_helper(n, 1));
 }
 
 /**
@@ -25,11 +25,11 @@ int _sqrt_recursion(int n)
  */
 int sqrt_helper(int n, int guess)
 {
-    if (guess * guess == n)
-        return (guess);
+	if (guess * guess == n)
+		return (guess);
 
-    if (guess * guess > n)
-        return (-1);
+	if (guess * guess > n)
+		return (-1);
 
-    return (sqrt_helper(n, guess + 1));
+	return (sqrt_helper(n, guess + 1));
 }
