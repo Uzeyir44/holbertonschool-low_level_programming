4d210c45027d009ebc6431ae52283ba07478f4bb
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-18 14:46:59 +0000

    betty

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -2,30 +2,44 @@
 
 int find_prime(int n, int divide, int count);
 
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: number to check
+ *
+ * Return: 1 if prime, 0 if not
+ */
 int is_prime_number(int n)
 {
-    if ( n <= 1 || find_prime(n, 2, 1) > 2)
-    {
-        return (0);
-    }
-    else
-    {
-        return (1);
-    }
+	if (n <= 1 || find_prime(n, 2, 1) > 2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
 }
 
+/**
+ * find_prime - helper function to count divisors
+ * @n: number to check
+ * @divide: current divisor
+ * @count: current count of divisors
+ *
+ * Return: total number of divisors
+ */
 int find_prime(int n, int divide, int count)
 {
-    if (divide == n)
-    {
-        return (count + 1);
-    }
-    else if (n % divide == 0)
-    {
-        return (find_prime(n, divide + 1, count + 1));
-    }
-    else
-    {
-        return (find_prime(n, divide + 1, count));
-    }
+	if (divide == n)
+	{
+		return (count + 1);
+	}
+	else if (n % divide == 0)
+	{
+		return (find_prime(n, divide + 1, count + 1));
+	}
+	else
+	{
+		return (find_prime(n, divide + 1, count));
+	}
 }
