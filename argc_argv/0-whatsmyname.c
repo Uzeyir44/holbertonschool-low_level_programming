7b3078c1e4782c6289539ff986bc4df6a992ec56
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-20 16:24:00 +0000

    betty

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,9 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - prints the last argument passed to the program
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0
+ */
 int main(int argc, char *argv[])
 {
-    printf("%s\n", argv[argc-1]);
+	printf("%s\n", argv[argc - 1]);
 
-    return (0);
+	return (0);
 }
