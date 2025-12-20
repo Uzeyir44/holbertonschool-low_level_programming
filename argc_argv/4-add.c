28fd09afebeba0dfeb99a80553cf8fd1e5044a87
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-20 17:51:39 +0000

    betty

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -2,35 +2,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - adds positive numbers passed as arguments
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success, 1 on error
+ */
 int main(int argc, char *argv[])
 {
-    int sum;
-    int i;
-    int j;
+	int sum = 0;
+	int i, j;
 
-    sum = 0;
-    
-    if (argc == 1)
-    {
-        printf("0\n");
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
 
-        return (0);
-    }
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
 
-    for (i = 1; i < argc; i++)
-    {
-        for (j = 0; argv[i][j] != '\0'; j++)
-        {
-            if (argv[i][j] < '0' || argv[i][j] > '9')
-            {
-                printf("Error\n");
-                return (1);
-            }
-        }
+	printf("%d\n", sum);
 
-        sum += atoi(argv[i]);
-    }
-
-    printf("%d\n", sum);
-    return (0);
+	return (0);
 }
