c3f5a96a536ce2dae8f7b7b05367f5e01b9142e3
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-25 06:29:24 +0000

    betty

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,36 +1,40 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
 
+/**
+ * _strdup - duplicates a string in newly allocated memory
+ * @str: string to duplicate
+ *
+ * Return: pointer to duplicated string, or NULL on failure
+ */
 char *_strdup(char *str)
 {
-    char *point;
-    int i;
-    int size = 0;
+	char *point;
+	int i;
+	int size = 0;
 
-    if (str == NULL)
-    {
-        return (NULL);
-    }
+	if (str == NULL)
+	{
+		return (NULL);
+	}
 
-    for (i = 0; *(str + i) != '\0'; i++)
-    {
-        size++;
-    }
+	for (i = 0; *(str + i) != '\0'; i++)
+	{
+		size++;
+	}
 
-    size++;
+	size++;
 
-    point = malloc(size * sizeof(char));
+	point = malloc(size * sizeof(char));
+	if (!point)
+	{
+		return (NULL);
+	}
 
-    if (!point)
-    {
-        return (NULL);
-    }
+	for (i = 0; i < size; i++)
+	{
+		point[i] = *(str + i);
+	}
 
-    for (i = 0; i < size; i++)
-    {
-        point[i] = *(str + i);
-    }
-
-    return (point);
-}
+	return (point);
+    
