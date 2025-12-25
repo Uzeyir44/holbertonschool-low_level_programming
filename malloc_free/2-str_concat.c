23e3cc180212fbfa494b986c76a86c975ca591d7
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-25 16:16:24 +0000

    betty

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,51 +1,54 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: pointer to newly allocated space containing concatenated string,
+ *         or NULL on failure
+ * Note: treats NULL strings as empty strings
+ */
 char *str_concat(char *s1, char *s2)
 {
-    int size1 = 0;
-    int size2 = 0;
-    int i, j;
-    char *pointer;
+	int size1 = 0;
+	int size2 = 0;
+	int i, j;
+	char *pointer;
 
-    if (s1 == NULL)
-    {
-        s1 = "";
-    }
-    
-    if (s2 == NULL)
-    {
-        s2 = "";
-    }
+	/* Treat NULL strings as empty strings */
+	if (s1 == NULL)
+		s1 = "";
 
-    while (s1[size1] != '\0')
-    {
-        size1++;
-    }
+	if (s2 == NULL)
+		s2 = "";
 
-    while (s2[size2] != '\0')
-    {
-        size2++;
-    }
+	/* Calculate length of s1 */
+	while (s1[size1] != '\0')
+		size1++;
 
-    pointer = malloc((size1 + size2 + 1) * sizeof(char));
-    
-    if (!pointer)
-    {
-        return (NULL);
-    }
+	/* Calculate length of s2 */
+	while (s2[size2] != '\0')
+		size2++;
 
-    for (i = 0; i < size1; i++)
-    {
-        pointer[i] = s1[i];
-    }
+	/* Allocate memory (+1 for null terminator) */
+	pointer = malloc((size1 + size2 + 1) * sizeof(char));
 
-    for (j = 0; j < size2; j++, i++)
-    {
-        pointer[i] = s2[j];
-    }
+	/* Return NULL if allocation fails */
+	if (pointer == NULL)
+		return (NULL);
 
-    pointer[i] = '\0';
+	/* Copy first string */
+	for (i = 0; i < size1; i++)
+		pointer[i] = s1[i];
 
-    return (pointer);
+	/* Copy second string */
+	for (j = 0; j < size2; j++, i++)
+		pointer[i] = s2[j];
+
+	/* Add null terminator */
+	pointer[i] = '\0';
+
+	return (pointer);
 }
