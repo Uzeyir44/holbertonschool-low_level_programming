b33839d2687a92126c1afc922cc82a14fffa5032
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-25 19:04:32 +0000

    betty

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,22 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
+ * Return: pointer to allocated memory
+ */
 void *malloc_checked(unsigned int b)
 {
-    void *point;
+	void *point;
 
-    point = malloc(b);
+	point = malloc(b);
 
-    if (!point)
-    {
-        exit(98);
-    }
+	if (!point)
+	{
+		exit(98);
+	}
 
-    return (point);
+	return (point);
 }
