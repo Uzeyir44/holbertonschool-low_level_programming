4e02d8b5c426a120644d4fa23c0ca694ba61be80
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-25 19:51:42 +0000

    task 2

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+    unsigned int size1 = 0;
+	unsigned int size2 = 0;
+	int i, j;
+	char *pointer;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[size1] != '\0')
+		size1++;
+
+	while (s2[size2] != '\0')
+		size2++;
+
+    if (size2 > n)
+    {
+        size2 = n;
+    }
+
+	pointer = malloc((size1 + size2 + 1) * sizeof(char));
+
+	if (pointer == NULL)
+		return (NULL);
+
+	for (i = 0; i < size1; i++)
+		pointer[i] = s1[i];
+
+	for (j = 0; j < size2; j++, i++)
+		pointer[i] = s2[j];
+
+	pointer[i] = '\0';
+
+	return (pointer);
+}
--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -3,5 +3,6 @@
 
 int _putchar(char c);  /* prototype for the provided function */
 void *malloc_checked(unsigned int b); /* allocates n bytes in the memmory */
+char *string_nconcat(char *s1, char *s2, unsigned int n); /* concatonates s1 and n first characters of s2 */
 
 #endif
