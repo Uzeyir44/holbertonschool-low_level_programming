d4b2913d68cf54f4a5f1dd808f180eb49f807c63
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-25 16:55:30 +0000

    task 4

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -6,5 +6,6 @@ char *create_array(unsigned int size, char c); /* creates an array with particul
 char *_strdup(char *str); /* creates a duplicate of a string */
 char *str_concat(char *s1, char *s2); /* creates a pointer to the  cocanotation of two strings */
 int **alloc_grid(int width, int height); /* creates two dimensial arrays */
+void free_grid(int **grid, int height); /* frees given row in array */
 
 #endif
