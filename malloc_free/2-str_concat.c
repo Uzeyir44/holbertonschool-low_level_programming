c7b35bc45c6bc0110257d04f915870e775db845b
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-25 16:13:11 +0000

    task 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -3,5 +3,49 @@
 
 char *str_concat(char *s1, char *s2)
 {
+    int size1 = 0;
+    int size2 = 0;
+    int i, j;
+    char *pointer;
+
+    if (s1 == NULL)
+    {
+        s1 = "";
+    }
     
+    if (s2 == NULL)
+    {
+        s2 = "";
+    }
+
+    while (s1[size1] != '\0')
+    {
+        size1++;
+    }
+
+    while (s2[size2] != '\0')
+    {
+        size2++;
+    }
+
+    pointer = malloc((size1 + size2 + 1) * sizeof(char));
+    
+    if (!pointer)
+    {
+        return (NULL);
+    }
+
+    for (i = 0; i < size1; i++)
+    {
+        pointer[i] = s1[i];
+    }
+
+    for (j = 0; j < size2; j++, i++)
+    {
+        pointer[i] = s2[j];
+    }
+
+    pointer[i] = '\0';
+
+    return (pointer);
 }
