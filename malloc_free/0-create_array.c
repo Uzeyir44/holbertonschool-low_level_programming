91c5c5641db2c08018f70bef4b374bc3c124a8a9
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-25 05:57:30 +0000

    wrong variable type

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -5,7 +5,9 @@
 char *create_array(unsigned int size, char c)
 {
     unsigned int i;
-    char arr[] = malloc(size * sizeof(char));
+    char *arr;
+
+    arr = malloc(size * sizeof(char));
 
     if (size <= 0)
     {
