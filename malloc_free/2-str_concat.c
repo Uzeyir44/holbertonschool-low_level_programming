5482cc78ec6e1c9ca1fe72e8e15a9c2b58d9aa04
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-25 19:54:55 +0000

    unecessary comments

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -17,37 +17,29 @@ char *str_concat(char *s1, char *s2)
 	int i, j;
 	char *pointer;
 
-	/* Treat NULL strings as empty strings */
 	if (s1 == NULL)
 		s1 = "";
 
 	if (s2 == NULL)
 		s2 = "";
 
-	/* Calculate length of s1 */
 	while (s1[size1] != '\0')
 		size1++;
 
-	/* Calculate length of s2 */
 	while (s2[size2] != '\0')
 		size2++;
 
-	/* Allocate memory (+1 for null terminator) */
 	pointer = malloc((size1 + size2 + 1) * sizeof(char));
 
-	/* Return NULL if allocation fails */
 	if (pointer == NULL)
 		return (NULL);
 
-	/* Copy first string */
 	for (i = 0; i < size1; i++)
 		pointer[i] = s1[i];
 
-	/* Copy second string */
 	for (j = 0; j < size2; j++, i++)
 		pointer[i] = s2[j];
 
-	/* Add null terminator */
 	pointer[i] = '\0';
 
 	return (pointer);
