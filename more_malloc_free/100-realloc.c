477641e5a5e7c0458f86d3dd759efb51917dbb5a
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-28 14:58:48 +0000

    task 4

---
+++ more_malloc_free/100-realloc.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+    void *point;
+    unsigned int i;
+    
+    if (!new_size && ptr)
+    {
+        free(ptr);
+        return (NULL);
+    }
+
+    if (!ptr)
+    {
+        ptr = malloc(new_size);
+        return (ptr);
+    }
+
+    if (new_size == old_size)
+    {
+        return (ptr);
+    }
+
+    point = malloc(new_size);
+    
+    for (i = 0; i < old_size; i++)
+    {
+        point[i] = ptr[i];
+    }
+
+    free(ptr);
+    return (point);
+}
--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -6,5 +6,6 @@ void *malloc_checked(unsigned int b); /* allocates n bytes in the memmory */
 char *string_nconcat(char *s1, char *s2, unsigned int n); /* concatonates s1 and n first characters of s2 */
 void *_calloc(unsigned int nmemb, unsigned int size); /* creates a pointer to array with nmemb elements each size bytes  */
 int *array_range(int min, int max); /* creates a pointer to an array with characters from min to max */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size); /* changes the size of memory */
 
 #endif
