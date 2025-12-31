a7591770ec503beaa14f020fa27a40e872cd8e91
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-31 15:34:58 +0000

    prototype

---
+++ structures_typedef/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);  /* prototype for the provided function */
+void init_dog(struct dog *d, char *name, float age, char *owner) /* initializes the variables */
+
+#endif
