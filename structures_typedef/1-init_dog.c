20545dd4d686980a4fd3da279edd882a9b6a57c3
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-31 15:55:36 +0000

    library declaration

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,6 +1,7 @@
 #include "dog.h"
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /* Initializes a dog structure */
 void init_dog(struct dog *d, char *name, float age, char *owner)
@@ -10,7 +11,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
         printf("Ok\n");
         exit(98);
     }
-    
+
     d->name = name;
     d->owner = owner;
 	d->age = age;
