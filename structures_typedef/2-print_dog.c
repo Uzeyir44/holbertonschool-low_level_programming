c44019f53579e45e362451c541e44cda47387bf7
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-31 16:15:20 +0000

    type error

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -15,7 +15,7 @@ void print_dog(struct dog *d)
     }
     else
     {
-        printf("Name: %c\n", d->name);
+        printf("Name: %s\n", d->name);
     }
     if (!(d->age))
     {
@@ -31,6 +31,6 @@ void print_dog(struct dog *d)
     }
     else
     {
-        printf("Owner: %c\n", d->owner);
+        printf("Owner: %s\n", d->owner);
     }
 }
