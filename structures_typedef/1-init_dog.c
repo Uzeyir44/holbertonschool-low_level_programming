973d6f44b5ef3f645766a5a05e0d8b8066dac518
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-31 15:39:42 +0000

    wrong prototype declaration

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "main.h"
+#include "dog.h"
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -2,6 +2,5 @@
 #define MAIN_H
 
 int _putchar(char c);  /* prototype for the provided function */
-void init_dog(struct dog *d, char *name, float age, char *owner) /* initializes the variables */
 
 #endif
