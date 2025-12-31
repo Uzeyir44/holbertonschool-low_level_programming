4c3a76b880ee623d2c9fb4293c8a4c6673cdbf94
Uzeyir44 <uzeirdzahangirov@gmail.com>
2025-12-31 15:14:31 +0000

    baetty

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - structure representing a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
-#endif
+#endif /* DOG_H */
