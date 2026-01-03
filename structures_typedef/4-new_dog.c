f2678a4ad59fbf15b36e97b9b47b2d89c565f7e0
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-03 21:32:43 +0000

    betty

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,56 +1,59 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    int i, j;
-    char *point_name;
-    char *point_owner;
-    dog_t *dog = malloc(sizeof(dog_t));
-    if (!dog)
-    {
-        return (NULL);
-    }
+	int i, j;
+	char *point_name;
+	char *point_owner;
+	dog_t *dog = malloc(sizeof(dog_t));
 
-    for (i = 0; *(name + i) != '\0'; i++);
-    for (j = 0; *(owner + j) != '\0'; j++);
-    dog->name = malloc(i + 1);
-    if (!(dog->name))
-    {
-        free(dog);
-        return (NULL);
-    }
+	if (!dog)
+	{
+		return (NULL);
+	}
 
-    point_name = dog->name;
+	for (i = 0; *(name + i) != '\0'; i++)
+		;
+	for (j = 0; *(owner + j) != '\0'; j++)
+		;
 
-    for (i = 0; *(name + i) != '\0'; i++)
-    {
-        *point_name  = name[i];
-        point_name++;
-    }
-    
-    *point_name = '\0';
-    
-    dog->owner = malloc(j + 1);
-    if (!(dog->owner))
-    {
-        free(dog->name);
-        free(dog);
-        return (NULL);
-    }
+	dog->name = malloc(i + 1);
+	if (!(dog->name))
+	{
+		free(dog);
+		return (NULL);
+	}
 
-    point_owner = dog->owner;
+	point_name = dog->name;
 
-    for (j = 0; *(owner + j) != '\0'; j++)
-    {
-        *point_owner  = owner[j];
-        point_owner++;
-    }
-    
-    *point_owner = '\0';
+	for (i = 0; *(name + i) != '\0'; i++)
+	{
+		*point_name = name[i];
+		point_name++;
+	}
 
-    dog->age = age;
+	*point_name = '\0';
 
-    return (dog);
+	dog->owner = malloc(j + 1);
+	if (!(dog->owner))
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+
+	point_owner = dog->owner;
+
+	for (j = 0; *(owner + j) != '\0'; j++)
+	{
+		*point_owner = owner[j];
+		point_owner++;
+	}
+
+	*point_owner = '\0';
+
+	dog->age = age;
+
+	return (dog);
 }
