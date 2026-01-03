75d3739600d594034ad630e0cea9caf65389759c
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-03 21:48:29 +0000

    betty

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,14 +1,16 @@
 #include "dog.h"
 #include <stdlib.h>
 
+/**
+ * free_dog - frees a dog
+ * @d: pointer to dog structure
+ */
 void free_dog(dog_t *d)
 {
-    if (!d)
-    {
-        exit(0);
-    }
+	if (!d)
+		exit(0);
 
-    free(d->name);
-    free(d->owner);
-    free(d);
+	free(d->name);
+	free(d->owner);
+	free(d);
 }
