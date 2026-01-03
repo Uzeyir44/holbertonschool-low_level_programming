598f7213dfb6db98ba352f4f0c2ecff7baa032e4
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-03 21:30:39 +0000

    again function restrictions

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -5,6 +5,8 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
     int i, j;
+    char *point_name;
+    char *point_owner;
     dog_t *dog = malloc(sizeof(dog_t));
     if (!dog)
     {
@@ -20,8 +22,16 @@ dog_t *new_dog(char *name, float age, char *owner)
         return (NULL);
     }
 
-    strcpy(dog->name, name);
+    point_name = dog->name;
 
+    for (i = 0; *(name + i) != '\0'; i++)
+    {
+        *point_name  = name[i];
+        point_name++;
+    }
+    
+    *point_name = '\0';
+    
     dog->owner = malloc(j + 1);
     if (!(dog->owner))
     {
@@ -30,7 +40,15 @@ dog_t *new_dog(char *name, float age, char *owner)
         return (NULL);
     }
 
-    strcpy(dog->owner, owner);
+    point_owner = dog->owner;
+
+    for (j = 0; *(owner + j) != '\0'; j++)
+    {
+        *point_owner  = owner[j];
+        point_owner++;
+    }
+    
+    *point_owner = '\0';
 
     dog->age = age;
 
