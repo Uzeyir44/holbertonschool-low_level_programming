3ed5ef923d48a0451687e6db94136f354feaf0f5
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-04 08:18:09 +0000

    betty

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -2,6 +2,7 @@
 #define FUNCTION_POINTERS_H
 
 int _putchar(char c);  /* prototype for the provided function */
-void print_name(char *name, void (*f)(char *)); /* calls the function with given parameter */
+/* calls the function with given parameter */
+void print_name(char *name, void (*f)(char *));
 
 #endif
