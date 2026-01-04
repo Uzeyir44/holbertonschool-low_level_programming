d4358ddd92632fb3da36440f73312663abe7354e
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-04 09:40:40 +0000

    betty

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -2,26 +2,33 @@
 #include <string.h>
 #include <stdlib.h>
 
+/**
+ * get_op_func - selects the correct function for an operator
+ * @s: operator passed as a string
+ *
+ * Return: pointer to the function corresponding to the operator,
+ * or NULL if no match is found
+ */
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-    while (ops[i].op != NULL)
-    {
-        if (strcmp(ops[i].op, s) == 0)
-        {
-            return (ops[i].f);
-        }
-        i++;
-    }
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
 
-    return (NULL);
+	return (NULL);
 }
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -3,23 +3,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * main - performs simple operations from command line
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 on success, exits with 98, 99, or 100 on error
+ */
 int main(int argc, char **argv)
 {
-    int (*op)(int, int);
+	int (*op)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    op = get_op_func(argv[2]);
-    if (!op)
-    {
-        printf("Error\n");
-        exit(99);
-    }
+	op = get_op_func(argv[2]);
+	if (!op)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-    printf("%d\n", op(atoi(argv[1]), atoi(argv[3])));
-    return (0);
+	printf("%d\n", op(atoi(argv[1]), atoi(argv[3])));
+	return (0);
 }
