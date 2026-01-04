5df622a83198264ea384055ef47fe472d02b2051
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-04 08:15:30 +0000

    betty

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,6 +1,14 @@
 #include "function_pointers.h"
 
+/**
+ * print_name - prints a name using a function pointer
+ * @name: name to be printed
+ * @f: pointer to function that prints the name
+ */
 void print_name(char *name, void (*f)(char *))
 {
-    f(name);
+	if (!name || !f)
+		return;
+
+	f(name);
 }
