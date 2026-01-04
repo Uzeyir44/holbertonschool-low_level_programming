293dde2f4ab833e9bf8175574d51bc6c9305ab7d
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-04 08:28:42 +0000

    library

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -5,6 +5,7 @@
 int _putchar(char c);
 /* calls the function with given parameter */
 void print_name(char *name, void (*f)(char *));
+#include <stddef.h>
 /* executes function for each element of array */
 void array_iterator(int *array, size_t size, void (*action)(int));
 
