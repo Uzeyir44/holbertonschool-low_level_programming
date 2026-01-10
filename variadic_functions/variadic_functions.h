24fc168c7edadeed6339c680fda3f25058c0be08
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-10 09:50:30 +0000

    betty

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);  /* prototype for the provided function */
+/* prototype for the provided function */
+int _putchar(char c);
 /* sums all the variables assigned to the function and returns its */
 int sum_them_all(const unsigned int n, ...);
 
