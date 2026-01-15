08473f015fc4f3ce5f1696e9a15033d64d3e4f96
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-15 10:51:36 +0000

    betty

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,35 +3,35 @@
 
 size_t print_list(const list_t *h)
 {
-    const list_t *temp1;
-    const list_t *temp2;
-    size_t node = 1;
+	const list_t *temp1;
+	const list_t *temp2;
+	size_t node = 1;
 
-    if (!h)
-    {
-        return (0);
-    }
+	if (!h)
+	{
+		return (0);
+	}
 
-    temp1 = h;
-    temp2 = h->next;
+	temp1 = h;
+	temp2 = h->next;
 
-    while (temp2 != NULL)
-    {
-        if (!(temp1->str))
-        {
-            printf("[0] (nil)\n");
-        }
-        else
-        {
-            printf("[%d] %s\n", temp1->len, temp1->str);
-        }
+	while (temp2 != NULL)
+	{
+		if (!(temp1->str))
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", temp1->len, temp1->str);
+		}
 
-        temp1 = temp2;
-        temp2 = temp2->next;
-        node++;
-    }
+		temp1 = temp2;
+		temp2 = temp2->next;
+		node++;
+	}
 
-    printf("[%d] %s\n", temp1->len, temp1->str);
+	printf("[%d] %s\n", temp1->len, temp1->str);
 
-    return (node);
+	return (node);
 }
