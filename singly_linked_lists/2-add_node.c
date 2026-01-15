f8e738cf75b5ae6a225f92e9fa3c180ef2dbec38
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-15 12:22:13 +0000

    betty

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -3,6 +3,12 @@
 #include <string.h>
 #include <stdlib.h>
 
+/**
+ * _strlen - Returns the length of a string
+ * @s: Pointer to the string
+ *
+ * Return: Length of the string
+ */
 int _strlen(const char *s)
 {
 	int i;
@@ -16,31 +22,38 @@ int _strlen(const char *s)
 	return (i);
 }
 
+/**
+ * add_node - Adds a new node at the beginning of a list
+ * @head: Double pointer to the head of the list
+ * @str: String to store in the new node
+ *
+ * Return: Address of the new element, or NULL if it fails
+ */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *temp;
-    
-    if (!(*head))
-    {
-        *head = malloc(sizeof(list_t));
-        if (!(*head))
-        {
-            return (NULL);
-        }
-        (*head)->str = strdup(str);
-        (*head)->len = _strlen(str);
-        (*head)->next = NULL;
-        return (*head);
-    }
+	list_t *temp;
 
-    temp = malloc(sizeof(list_t));
-    if (!temp)
-    {
-        return (NULL);
-    }
-    temp->next = *head;
-    temp->str = strdup(str);
-    temp->len = _strlen(str);
-    *head = temp;
-    return (*head);
+	if (!(*head))
+	{
+		*head = malloc(sizeof(list_t));
+		if (!(*head))
+		{
+			return (NULL);
+		}
+		(*head)->str = strdup(str);
+		(*head)->len = _strlen(str);
+		(*head)->next = NULL;
+		return (*head);
+	}
+
+	temp = malloc(sizeof(list_t));
+	if (!temp)
+	{
+		return (NULL);
+	}
+	temp->next = *head;
+	temp->str = strdup(str);
+	temp->len = _strlen(str);
+	*head = temp;
+	return (*head);
 }
