c90376128065200e6c2fa06189cdbb1b3902789a
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-15 11:26:29 +0000

    betty

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,23 +1,29 @@
 #include "lists.h"
 #include <stdio.h>
 
+/**
+ * list_len - Returns the number of elements in a linked list
+ * @h: Pointer to the head of the list
+ *
+ * Return: Number of nodes in the list
+ */
 size_t list_len(const list_t *h)
 {
-    const list_t *temp;
-    size_t num = 1;
+	const list_t *temp;
+	size_t num = 1;
 
-    if (!h)
-    {
-        return (0);
-    }
+	if (!h)
+	{
+		return (0);
+	}
 
-    temp = h;
+	temp = h;
 
-    while (temp->next != NULL)
-    {
-        num++;
-        temp = temp->next;
-    }
+	while (temp->next != NULL)
+	{
+		num++;
+		temp = temp->next;
+	}
 
-    return (num);
+	return (num);
 }
