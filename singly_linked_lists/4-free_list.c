ff6f6db31030a3158176964f9af02d3cacdfa122
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-16 09:25:47 +0000

    error

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -8,7 +8,7 @@ void free_list(list_t *head)
     while (head != NULL)
     {
         temp = head->next;
-        free(*head->str);
+        free(head->str);
         free(head);
         head = temp;
     }
