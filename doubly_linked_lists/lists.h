a6edd080e23205f314f16c6ebf8ea1a0dd6866ad
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-16 11:34:49 +0000

    betty

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -22,7 +22,6 @@ typedef struct dlistint_s
 } dlistint_t;
 /* print_dlistint - prints all the data in a dlistint_t list */
 size_t print_dlistint(const dlistint_t *h);
-/* dlistint_len - returns the number of elements in a dlistint_t list */
-size_t dlistint_len(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h); /* dlistint_len - returns the number of elements in a dlistint_t list */
 
 #endif
