91e6b868704bddfa60d61aec523993b4bd59c3af
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-16 11:18:53 +0000

    mixed declaration and code

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -4,10 +4,8 @@
 size_t print_dlistint(const dlistint_t *h)
 {
     const dlistint_t *temp;
+    size_t num = 1;
     temp = h;
-    size_t num;
-
-    num = 1;
 
     while (temp->next != NULL)
     {
