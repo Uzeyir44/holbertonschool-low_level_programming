afbadad81e89fea447ca7138a160335d35a2f0cc
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-16 12:00:35 +0000

    betty

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -2,36 +2,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * add_dnodeint - adds a new node at the beginning of a dlistint_t list
+ * @head: double pointer to the head of the list
+ * @n: integer to store in the new node
+ *
+ * Return: address of the new node, or NULL if it failed
+ */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *temp;
-    
-    if (!(*head))
-    {
-        *head = malloc(sizeof(dlistint_t));
+	dlistint_t *temp;
 
-        if (!(*head))
-        {
-            return (NULL);
-        }
+	if (!(*head))
+	{
+		*head = malloc(sizeof(dlistint_t));
+		if (!(*head))
+			return (NULL);
 
-        (*head)->n = n;
-        (*head)->next = NULL;
-        (*head)->prev = NULL;
+		(*head)->n = n;
+		(*head)->next = NULL;
+		(*head)->prev = NULL;
 
-        return (*head);
-    }
+		return (*head);
+	}
 
-    temp = malloc(sizeof(dlistint_t));
-    if (!temp)
-    {
-        return (NULL);
-    }
+	temp = malloc(sizeof(dlistint_t));
+	if (!temp)
+		return (NULL);
 
-    temp->n = n;
-    temp->next = *head;
-    (*head)->prev = temp;
-    temp->prev = NULL;
-    *head = temp;
-    return (temp);
+	temp->n = n;
+	temp->next = *head;
+	(*head)->prev = temp;
+	temp->prev = NULL;
+	*head = temp;
+
+	return (temp);
 }
