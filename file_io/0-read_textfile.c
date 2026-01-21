ea628b6ce6b031e950453ecea4a103fd9c70c3ae
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-21 10:44:08 +0000

    betty

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -4,23 +4,35 @@
 #include <fcntl.h>
 #include <stdlib.h>
 
+/**
+ * read_textfile - Reads a text file and prints it to stdout
+ * @filename: Name of the file
+ * @letters: Number of letters to read and print
+ *
+ * Return: Number of letters read and printed, or 0 on failure
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
     int fd;
-    char *buf = malloc(letters);
+    char *buf;
     ssize_t bytes_read;
     ssize_t bytes_written;
-    
+
     if (filename == NULL)
         return (0);
 
-    fd = open(filename, O_RDONLY);
-    
-    if (fd == -1)
+    buf = malloc(letters);
+    if (buf == NULL)
         return (0);
-    
-    bytes_read = read(fd, buf, letters);
 
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+    {
+        free(buf);
+        return (0);
+    }
+
+    bytes_read = read(fd, buf, letters);
     if (bytes_read == -1)
     {
         free(buf);
@@ -32,8 +44,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
     free(buf);
     close(fd);
 
-    if (bytes_read != bytes_written)
+    if (bytes_written != bytes_read)
         return (0);
-    
+
     return (bytes_written);
 }
