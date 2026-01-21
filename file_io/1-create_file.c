725faeba40a5278ae678cda8de35c021a0fef709
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-21 11:05:42 +0000

    added function that returns the length of the string

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -2,6 +2,15 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+int len(char *text)
+{
+    int i;
+
+    for (i = 0; text[i] != '\0'; i++);
+
+    return (i);
+}
+
 int create_file(const char *filename, char *text_content)
 {
     int fd;
@@ -20,7 +29,7 @@ int create_file(const char *filename, char *text_content)
     }
     else
     {
-        write(fd, text_content, 1);
+        write(fd, text_content, len(text_content));
         return (1);
     }
 }
