85c9a5514a6addc7ffa69c81d4ca2ed7504f505b
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-21 10:45:30 +0000

    betty

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -13,39 +13,39 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    char *buf;
-    ssize_t bytes_read;
-    ssize_t bytes_written;
+	int fd;
+	char *buf;
+	ssize_t bytes_read;
+	ssize_t bytes_written;
 
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+		return (0);
 
-    buf = malloc(letters);
-    if (buf == NULL)
-        return (0);
+	buf = malloc(letters);
+	if (buf == NULL)
+		return (0);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-    {
-        free(buf);
-        return (0);
-    }
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buf);
+		return (0);
+	}
 
-    bytes_read = read(fd, buf, letters);
-    if (bytes_read == -1)
-    {
-        free(buf);
-        close(fd);
-        return (0);
-    }
+	bytes_read = read(fd, buf, letters);
+	if (bytes_read == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
 
-    bytes_written = write(STDOUT_FILENO, buf, bytes_read);
-    free(buf);
-    close(fd);
+	bytes_written = write(STDOUT_FILENO, buf, bytes_read);
+	free(buf);
+	close(fd);
 
-    if (bytes_written != bytes_read)
-        return (0);
+	if (bytes_written != bytes_read)
+		return (0);
 
-    return (bytes_written);
+	return (bytes_written);
 }
