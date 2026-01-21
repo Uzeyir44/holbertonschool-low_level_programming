74f156ee2c36a438b57e3bfde75ba0f29319e663
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-21 11:20:53 +0000

    betty

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -18,25 +18,29 @@ int len(char *text)
 	return (i);
 }
 
+/**
+ * append_text_to_file - Appends text at the end of a file
+ * @filename: Name of the file
+ * @text_content: String to append
+ *
+ * Return: 1 on success, -1 on failure
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd;
+	int fd;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    fd = open(filename, O_WRONLY | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
 
-    if (fd == -1)
-        return (-1);
+	if (text_content == NULL)
+		return (1);
 
-    if (text_content == NULL)
-    {
-        return (1);
-    }
-    else
-    {
-        write(fd, text_content, len(text_content));
-        return (1);
-    }
+	write(fd, text_content, len(text_content));
+	close(fd);
+
+	return (1);
 }
