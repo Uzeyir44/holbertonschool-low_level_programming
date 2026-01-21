38185062f06917c4e581af28c2ffb0d9d0c041a9
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-21 14:23:16 +0000

    impovememnt

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -2,9 +2,23 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
-#include <stdio.h>
 
 #define BUF_SIZE 1024
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ *
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
 
 /**
  * main - copies content of a file to another file
@@ -21,21 +35,25 @@ int main(int argc, char **argv)
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		write(1, "Usage: cp file_from file_to\n", 28);
 		exit(97);
 	}
 
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		write(1, "Error: Can't read from file ", 28);
+		write(1, argv[1], _strlen(argv[1]));
+		write(1, "\n", 1);
 		exit(98);
 	}
 
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		write(1, "Error: Can't write to ", 22);
+		write(1, argv[2], _strlen(argv[2]));
+		write(1, "\n", 1);
 		close(fd_from);
 		exit(99);
 	}
@@ -45,7 +63,9 @@ int main(int argc, char **argv)
 		w = write(fd_to, buffer, r);
 		if (w == -1 || w != r)
 		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			write(1, "Error: Can't write to ", 22);
+			write(1, argv[2], _strlen(argv[2]));
+			write(1, "\n", 1);
 			close(fd_from);
 			close(fd_to);
 			exit(99);
@@ -54,7 +74,9 @@ int main(int argc, char **argv)
 
 	if (r == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		write(1, "Error: Can't read from file ", 28);
+		write(1, argv[1], _strlen(argv[1]));
+		write(1, "\n", 1);
 		close(fd_from);
 		close(fd_to);
 		exit(98);
@@ -62,13 +84,17 @@ int main(int argc, char **argv)
 
 	if (close(fd_from) == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_from);
+		write(1, "Error: Can't close fd ", 22);
+		/* convert fd to string if needed, else print single byte placeholder */
+		write(1, "\n", 1);
 		exit(100);
 	}
 
 	if (close(fd_to) == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_to);
+		write(1, "Error: Can't close fd ", 22);
+		/* convert fd to string if needed, else print single byte placeholder */
+		write(1, "\n", 1);
 		exit(100);
 	}
 
