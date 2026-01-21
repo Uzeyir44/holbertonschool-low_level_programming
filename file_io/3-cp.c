05e41b3a5c0332d2e9e145cc0d6c43085f85a1a5
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-21 13:39:35 +0000

    debuging of the code

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -3,64 +3,49 @@
 #include <fcntl.h>
 #include <stdlib.h>
 
-int file_from_open(char *filename, int checker)
+void file_from_open(char *filename, int checker)
 {
     if (checker == -1)
     {
-        exit(98);
         dprintf(2, "Error: Can't read from file %s\n", filename);
-        return (1);
-    }
-
-    return (0);
-}
-
-int file_from_read(char *filename, ssize_t checker)
-{
-    if (checker == -1)
-    {
         exit(98);
+    }
+}
+
+void file_from_read(char *filename, ssize_t checker)
+{
+    if (checker == -1)
+    {
         dprintf(2, "Error: Can't read from file %s\n", filename);
-        return (1);
+        exit(98);
     }
-
-    return (0);
 }
 
-int file_to_open(char *filename, int checker)
+void file_to_open(char *filename, int checker)
 {
     if (checker == -1)
     {
-        exit(99);
         dprintf(2, "Error: Can't write to %s\n", filename);
-        return (1);
+        exit(99);
     }
-
-    return (0);
 }
 
-int file_to_write(char *filename, ssize_t checker)
+void file_to_write(char *filename, ssize_t checker)
 {
     if (checker == -1)
     {
-        exit(99);
         dprintf(2, "Error: Can't write to %s\n", filename);
-        return (1);
+        exit(99);
     }
-
-    return (0);
 }
 
-int close_checker(int descrip, int close_value)
+void close_checker(int descrip, int close_value)
 {
     if (close_value == -1)
     {
+        dprintf(2, "Error: Can't close fd %d\n", descrip);
         exit(100);
-        dprintf(2, "Error: Can't close fd %d", descrip);
-        return (1);
     }
-
-    return (0);
 }
 
 void cp(int argc, char **argv)
@@ -68,36 +53,31 @@ void cp(int argc, char **argv)
     char buf[1024];
     int fd1;
     int fd2;
-    ssize_t read;
-    ssize_t write;
-    int close;
+    ssize_t read1;
+    ssize_t write2;
+    int close_file;
 
     if (argc != 3)
     {
-        exit(97);
         dprintf(2, "Usage: cp file_from file_to\n");
+        exit(97);
     }
 
     fd1 = open(argv[1], O_RDONLY);
-    if (file_from_o_r(argv[1], fd1))
-        return;
+    file_from_open(argv[1], fd1);
 
     fd2 = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
-    if (file_to_open(argv[2], fd2))
-        return;
+    file_to_open(argv[2], fd2);
 
-    read = read(fd1, buf, 1024);
-    if (file_from_read(argv[1], read))
-        return;
+    read1 = read(fd1, buf, 1024);
+    file_from_read(argv[1], read1);
     
-    write = write(fd2, buf, read);
-    if (file_to_write(argv[2], write));
-        return;
+    write2 = write(fd2, buf, read1);
+    file_to_write(argv[2], write1);
 
-    close = close(fd1);
-    if (close_checker(fd1, close))
-        return;
-    close = close(fd2);
-    if (close_checker(fd2, close))
-        return;
+    close_file = close(fd1);
+    close_checker(fd1, close_file);
+    
+    close_file = close(fd2);
+    close_checker(fd2, close_file);
 }
