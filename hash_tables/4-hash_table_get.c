e03188249b6ee53c8969d66d383f00ebc19cc668
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-22 14:15:26 +0000

    NULL checker

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -7,6 +7,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
     hash_node_t *node;
     unsigned long int index;
 
+    if (ht == NULL)
+        return (NULL);
+
     index = key_index((const unsigned char *)key, ht->size);
     node = ht->array[index];
     
