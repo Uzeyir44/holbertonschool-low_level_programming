294ad482469632c0445384ac99ed0ff478fad9e7
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-22 11:36:07 +0000

    forgot to comment one variable

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -4,7 +4,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
     hash_table_t *new_table = malloc(sizeof(hash_table_t));
-    unsigned long int i;
+    // unsigned long int i;
     
     if (new_table == NULL)
         return (NULL);
