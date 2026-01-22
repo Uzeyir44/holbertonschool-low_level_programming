f3eb0ec5c21680b1acb3e4742860c98c75803122
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-22 14:02:11 +0000

    betty

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -2,23 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * equal_key - Checks if a key exists and updates its value
+ * @node: Pointer to the first node in the list
+ * @key: Key to compare
+ * @value: New value to assign
+ *
+ * Return: 1 if key was found and updated,
+ *         0 if memory allocation failed,
+ *         -1 if key was not found
+ */
 int equal_key(hash_node_t *node, const char *key, const char *value)
 {
-    while (node)
-    {
-        if (strcmp(node->key, key) == 0)
-        {
-            free(node->value);
-            node->value = strdup(value);
-            if (node->value == NULL)
-                return (0);
-            return (1);
-        }
+	while (node)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			free(node->value);
+			node->value = strdup(value);
+			if (node->value == NULL)
+				return (0);
+			return (1);
+		}
+		node = node->next;
+	}
 
-        node = node->next;
-    }
-
-    return (-1);
+	return (-1);
 }
 
 /**
@@ -41,7 +50,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	node = ht->array[index];
 
 	if (equal_key(node, key, value) == 1)
-        return (1);
+		return (1);
 
 	tmp = malloc(sizeof(hash_node_t));
 	if (!tmp)
