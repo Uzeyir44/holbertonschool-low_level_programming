e92ccf4eb74d8e3eee00b19644205b92b44cb8f8
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-22 14:49:49 +0000

    task 6

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_delete - Deletes a hash table
+ * @ht: Pointer to the hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node, *next;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+
+		while (node)
+		{
+			free(node->key);
+			free(node->value);
+			next = node->next;
+			free(node);
+			node = next;
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -42,5 +42,7 @@ int equal_key(hash_node_t *node, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 /* prints the hash table */
 void hash_table_print(const hash_table_t *ht);
+/* deletes the whole hash table */
+void hash_table_delete(hash_table_t *ht);
 
 #endif /* HASH_TABLES_H */
