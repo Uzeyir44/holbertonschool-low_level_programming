008875f764cfa2ec3959d8ae09c1f3d86f1ee25c
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-22 12:49:25 +0000

    checking why error happens

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -27,17 +27,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
         free(tmp);
         return (0);
     }
-    
-    if (key != ht->array[index]->key)
-    {
-        tmp->next = ht->array[index];
-        ht->array[index] = tmp;
-    }
-    else
-    {
-        tmp->next = NULL;
-        ht->array[index] = tmp;
-    }
+
+    tmp->next = ht->array[index];
+    ht->array[index] = tmp;
 
     return (1);
 }
