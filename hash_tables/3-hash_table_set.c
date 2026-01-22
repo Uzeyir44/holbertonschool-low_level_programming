ff6a364359871a86390059d302a932fb5f54306e
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-22 12:40:42 +0000

    added string header and reduced code

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,21 +1,26 @@
 #include "hash_tables.h"
 #include <stdlib.h>
+#include <string.h>
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
     unsigned long int index;
     hash_node_t *tmp = malloc(sizeof(hash_node_t));
+
     if (tmp == NULL)
         return (0);
 
-    index = key_index(key, ht->size);
+    index = key_index((const unsigned char *)key, ht->size);
     tmp->key = strdup(key);
+
     if (tmp->key == NULL)
     {
         free(tmp);
         return (0);
     }
+
     tmp->value = strdup(value);
+
     if (tmp->value == NULL)
     {
         free(tmp->key);
@@ -23,17 +28,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
         return (0);
     }
 
-    tmp->next = NULL;
-
-    if (ht->array[index] == NULL)
-    {
-        ht->array[index] = tmp;
-    }
-    else
-    {
-        tmp->next = ht->array[index];
-        ht->array[index] = tmp;
-    }
+    tmp->next = ht->array[index];
+    ht->array[index] = tmp;
 
     return (1);
 }
