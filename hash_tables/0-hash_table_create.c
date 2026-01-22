7612585a1572e7bc6027e39e7d33e2d6ec01546e
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-22 11:21:19 +0000

    malloc

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,7 +1,8 @@
 #include "hash_tables.h"
+#include <stdlib.h>
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *new_table[size];
+    hash_table_t *new_table = malloc(size);
     return (new_table);
 }
